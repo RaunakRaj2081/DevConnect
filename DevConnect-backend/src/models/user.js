2a6eb5d43352620079ea1e88d485a37fb4b10d89
RaunakRaj2081 <raunakraj.2081@gmail.com>
2026-09-19 19:59:55 +0530

    code updates

--- DevConnect-backend/src/models/user.js
+++ DevConnect-backend/src/models/user.js
@@ -84,7 +84,7 @@ userSchema.methods.getJWT = async function () {
   const user = this;
 
   const token = await jwt.sign({ _id: user._id }, "DEV@Connect$790", {
-    expiresIn: "7d",
+    expiresIn: "1d",
   });
 
   return token;
---
+++ DevConnect-backend/src/utils/razorpay.js
@@ -0,0 +1,12 @@
+const Razorpay = require("razorpay");
+
+let instance = null;
+
+if (process.env.RAZORPAY_KEY_ID && process.env.RAZORPAY_KEY_SECRET) {
+    instance = new Razorpay({
+        key_id: process.env.RAZORPAY_KEY_ID,
+        key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+}
+
+module.exports = instance;
